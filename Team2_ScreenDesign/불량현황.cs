0cf16ce3af641eb176d05a205f1746b2c8c9716a
esp7789 <gjwlstjd203@naver.com>
2020-01-02 15:58:08 +0900

    gogo

---
+++ Team2_ScreenDesign/불량현황.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BikeCompany.쥔성쥔성
+{
+    public partial class 불량현황 : Form
+    {
+        public 불량현황()
+        {
+            InitializeComponent();
+        }
+    }
+}
