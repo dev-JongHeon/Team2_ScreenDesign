3788c3a823bfc2cca7aaf74052552512e07494c2
car2300 <audrms03@naver.com>
2020-01-07 13:24:34 +0900

    최명근

--- Team2_ScreenDesign/Forms/KJH/MainForm.cs
+++ Team2_ScreenDesign/Forms/KJH/MainForm.cs
@@ -42,7 +42,7 @@ namespace Team2_ScreenDesign
         }
         private void 새로고침ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenBaseForm<Base1Dgv>("테스트");
+            OpenBaseForm<Defective>("테스트");
         }
         private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
         {
