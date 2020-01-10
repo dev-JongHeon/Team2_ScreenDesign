8b76e502b84fa8f28e2ec0e41c74082c417ac654
esp7789 <gjwlstjd203@naver.com>
2020-01-10 16:52:27 +0900

    rrr

---
+++ Team2_ScreenDesign/SettingMenuStrip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team2_ScreenDesign
+{
+    public class SettingMenuStrip
+    {
+        public void SetMenu<T>(T frm) where T : BaseForm, new()
+        {
+            
+        }
+
+        public void UnsetMenu()
+        {
+
+
+        }
+    }
+}
