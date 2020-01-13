d29cc9e9952ceb5ab57e5fbaa193b1638001f6bc
종헌 김 <bluemoon1510@naver.com>
2020-01-13 10:31:31 +0900

    2

---
+++ Team2_ScreenDesign/Forms/KJH/ProductionProcessAdd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team2_ScreenDesign
+{
+    public partial class ProductionProcessAdd : Team2_ScreenDesign.BasePopup
+    {
+        public ProductionProcessAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Team2_ScreenDesign/Forms/KJH/WorkerbyProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team2_ScreenDesign
+{
+    public partial class WorkerbyProcess : BasePopup
+    {
+        public WorkerbyProcess()
+        {
+            InitializeComponent();
+            searchUserControl1.Labelname = "";
+        }
+        
+        
+
+    }
+}
